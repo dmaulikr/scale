609231c06d0bb51f9981edd50685c2c173fe3f49
neal <caoyaha@cloudoc.cn>
2017-09-12 19:16:33 +0800

    Add 3d touch

--- Scale/AppDelegate.swift
+++ Scale/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+enum Shortcut:String {
+    case openBlue = "OpenBlue"
+}
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,10 +19,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
         [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        print("didFinishLaunchingWithOptions called")
+        var isLaunchedFromQuickAction = false
+        
+        // Check if it's launched from Quick Action
+        if let shortcutItem = launchOptions?[UIApplicationLaunchOptionsKey.shortcutItem] as? UIApplicationShortcutItem {
             
-        self.window?.backgroundColor = UIColor.white
-
-        return true
+            isLaunchedFromQuickAction = true
+            // Handle the sortcutItem
+            handleQuickAction(shortcutItem)
+        } else {
+            self.window?.backgroundColor = UIColor.white
+        }
+        
+        // Return false if the app was launched from a shortcut, so performAction... will not be called.
+        return !isLaunchedFromQuickAction
     }
     
     func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
@@ -49,6 +64,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        completionHandler(handleQuickAction(shortcutItem))
+    }
 
+    func handleQuickAction(_ shortcutItem: UIApplicationShortcutItem) -> Bool {
+     
+        var quickActionHandled = false
+        let type  = shortcutItem.type.components(separatedBy: ".").last!
+        if let shortcutType = Shortcut.init(rawValue: type) {
+            switch shortcutType {
+            case .openBlue:
+                self.window?.backgroundColor = UIColor(red: 151.0/255.0, green: 187.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+                quickActionHandled = true
+            }
+        }
+        
+        return quickActionHandled
+    }
 }
 
--- Scale/ViewController.swift
+++ Scale/ViewController.swift
@@ -19,7 +19,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if let touch = touches.first {
+            if #available(iOS 9.0, *) {
+                if traitCollection.forceTouchCapability == .available {
+                    // 3d touch capable
+                    let force = touch.force/touch.maximumPossibleForce
+                    forceLabel.text = "\(force) % force"
+                    
+                    if touch.force >= touch.maximumPossibleForce {
+                        forceLabel.text = "385+  grams"
+                    }else {
+                        let force = touch.force/touch.maximumPossibleForce
+                        let grams = force * 385
+                        let roundGrams = Int(grams)
+                        forceLabel.text = "\(roundGrams) grams"
+                    }
+                }
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        forceLabel.text = "0 grams"
+    }
 }
+
+
 
